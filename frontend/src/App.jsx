0f072a4fdc54b4e8cc6a45a4e4da7c203cdd980a
CornedeLange <u23788862@tuks.co.za>
2025-05-23 17:21:56 +0200

    Added Searching Functionality, also updated some styling for products

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,48 @@
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import ViewProduct from './components/product/ViewProduct';
 import Home from './pages/Home';
 import Splash from './pages/Splash';
 import Login from './pages/Login';
+import SearchResults from './pages/SearchResults';
 import Navigation from './components/navigation/Navigation';
+import { SearchProvider } from './components/search/SearchProvider';
 
 //APP Will also be used to define the routes for the application
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        {/* pages routes */}
-        <Route path="/" element={<Splash />} />
-        <Route path="/Home" element={<><Navigation /><Home /></>}/>
-        <Route path="/Login" element={<Login />}/>
-        {/* components routes */}
-        <Route path="/Product/:id" element={<><Navigation /><ViewProduct /></>} />
-      </Routes>
-    </div>
-  )
+    <SearchProvider>
+      <div className="App">
+        <Routes>
+          {/* pages routes */}
+          <Route path="/" element={<Splash />} />
+          <Route path="/Home" element={
+            <React.Fragment key="home">
+              <Navigation />
+              <Home />
+            </React.Fragment>
+          } />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/search" element={
+            <React.Fragment key="search">
+              <Navigation />
+              <SearchResults />
+            </React.Fragment>
+          } />
+          {/* components routes */}
+          <Route path="/Product/:id" element={
+            <React.Fragment key="product">
+              <Navigation />
+              <ViewProduct />
+            </React.Fragment>
+          } />
+          {/* Handle 404s */}
+          <Route path="*" element={<Navigate to="/Home" replace />} />
+        </Routes>
+      </div>
+    </SearchProvider>
+  );
 }
 
 export default App;
