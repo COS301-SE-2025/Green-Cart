5447eac13a1d59d1a4c07c859e360ccd7ff36b4a
Samvit Prakash <samvitprakash2004@gmail.com>
2025-06-24 01:21:13 +0200

    Complete Integration of the Sustainablity ratings into the products viewing page

--- frontend/src/product-services/fetchProduct.js
+++ frontend/src/product-services/fetchProduct.js
@@ -1,6 +1,6 @@
 const API_URL = "http://localhost:8000/products/FetchProduct";
 
-export const fetchProduct = async ({ apiKey, product_id }) => {
+export const fetchProduct = async ({ product_id }) => {
     try {
         const response = await fetch(API_URL, {
             method: "POST",
@@ -8,7 +8,6 @@ export const fetchProduct = async ({ apiKey, product_id }) => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                apiKey,
                 product_id
             })
         });
