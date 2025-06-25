014441df3a3f21645e80b48be5434750dfdc7636
Samvit Prakash <samvitprakash2004@gmail.com>
2025-06-25 16:43:33 +0200

    Small syntax fix from merging error

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -44,7 +44,7 @@ export default function Cart() {
                 >
                   Remove
                 </button>
-              </li>
+              </div>
             ))}
           </ul>
 
