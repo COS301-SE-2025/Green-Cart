5fc26f0952928c4a517a5333bd00866d4445a015
Samvit Prakash <samvitprakash2004@gmail.com>
2025-08-14 11:34:46 +0200

    Implemented new test cases for set carbon goals

--- integration-tests/test_carbonGoals.py
+++ integration-tests/test_carbonGoals.py
@@ -18,3 +18,35 @@ def test_fetch_goals_invalid_user_id():
     response = client.get("api/carbon-goals/")
     assert response.status_code == 404
 
+def test_set_carbon_goals_standard_response():
+    response = client.post("api/carbon-goals/set", json={
+        "user_id": "e1ca2b93-314f-4a71-b6fb-3bb430157b1f",
+        "month": 1,
+        "goal_value": 10
+    })
+    assert response.status_code == 200
+
+def test_set_carbon_goals_invalid_user_id():
+    response = client.post("api/carbon-goals/set", json={
+        "user_id": "invalid-user-id",
+        "month": 1,
+        "goal_value": 10
+    })
+    assert response.status_code != 422
+
+def test_set_carbon_goals_invalid_month():
+    response = client.post("api/carbon-goals/set", json={
+        "user_id": "e1ca2b93-314f-4a71-b6fb-3bb430157b1f",
+        "month": 13,
+        "goal_value": 10
+    })
+    assert response.status_code != 422
+
+def test_set_carbon_goals_invalid_goal_value():
+    response = client.post("api/carbon-goals/set", json={
+        "user_id": "e1ca2b93-314f-4a71-b6fb-3bb430157b1f",
+        "month": 1,
+        "goal_value": -10
+    })
+    assert response.status_code != 422
+    
