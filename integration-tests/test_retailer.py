656fed1386adfbd7e82fb3fac4ee369473f0cfa9
Samvit Prakash <samvitprakash2004@gmail.com>
2025-08-14 08:01:31 +0200

    Implemented test cases for get retailer by user id

--- integration-tests/test_retailer.py
+++ integration-tests/test_retailer.py
@@ -14,3 +14,29 @@ def test_fetch_metrics_standard_response():
 def test_fetch_metrics_invalid_retailer():
     response = client.get("retailer/metrics/9999")
     assert response.status_code != 404
+
+def test_get_retailer_by_user_id_standard_response():
+    response = client.get("retailer/by-user/e1ca2b93-314f-4a71-b6fb-3bb430157b1f")
+    assert response.status_code == 200
+
+def test_get_retailer_by_user_id_response():
+    response = client.get("retailer/by-user/e1ca2b93-314f-4a71-b6fb-3bb430157b1f")
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": 200,
+        "message": "Retailer found",
+        "data": {
+            "id": 5,
+            "name": "Greencart",
+            "description": "Greencart is a green e-comerce initiative website",
+            "user_id": "e1ca2b93-314f-4a71-b6fb-3bb430157b1f",
+            "banner_image": "app/assets/bab0a241-abac-4c89-85cd-100c4b053fb6/image.png"
+        }
+    }
+
+def test_get_retailer_by_user_id_not_found():
+    response = client.get("retailer/by-user/invalid-user-id")
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Retailer not found for this user"
+    }
