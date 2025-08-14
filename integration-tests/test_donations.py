eb30064b9d6f808e0c7095d86699d11a4a956c75
Samvit Prakash <samvitprakash2004@gmail.com>
2025-08-14 07:30:12 +0200

    Added new test case for donations (invalid bodies)

--- integration-tests/test_donations.py
+++ integration-tests/test_donations.py
@@ -15,3 +15,32 @@ def test_donations_standard_response():
         "donation_amount": 10
     })
     assert response.status_code == 200
+
+def test_donations_invalid_body_structure():
+    response = client.post("/donations/apply", json={
+        "user_id": "invalid-id",
+        "base_carbon_footprint": 100.0,
+        "donation_amount": 10
+    })
+    assert response.status_code == 422
+
+    response = client.post("/donations/apply", json={
+        "cart_id": 0,
+        "base_carbon_footprint": 100.0,
+        "donation_amount": 10
+    })
+    assert response.status_code == 422
+
+    response = client.post("/donations/apply", json={
+        "user_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "cart_id": 0,
+        "donation_amount": 10
+    })
+    assert response.status_code == 422
+
+    response = client.post("/donations/apply", json={
+        "user_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "cart_id": 0,
+        "base_carbon_footprint": 100.0
+    })
+    assert response.status_code == 422
