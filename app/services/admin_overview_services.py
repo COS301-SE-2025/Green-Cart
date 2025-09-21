b826ef110a21d621f2d13a1f02e4ac74029bbd38
Samvit Prakash <samvitprakash2004@gmail.com>
2025-09-21 22:00:48 +0200

    Implement get_orders_list function to fetch detailed order information including user email and address

--- app/services/admin_overview_services.py
+++ app/services/admin_overview_services.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm import Session
 from app.models.orders import Order
+from app.models.user import User
+from app.models.address import Address
 from datetime import date
 from datetime import timedelta
 from sqlalchemy import extract
@@ -48,4 +50,31 @@ def get_orders_overview(request, db: Session):
         "total_delivered": TotalDelivered,
         "total_cancelled": TotalCancelled,
         "monthly_comparison": percentage_increase
+    }
+
+def get_orders_list(db: Session):
+    orders = db.query(Order).order_by(Order.id.desc()).all()
+    orders_list = []
+    for order in orders:
+        username = db.query(User.email).filter(User.id == order.user_id).first()
+        user_email = username[0] if username else "unknown@example.com"
+        address = db.query(Address).filter(Address.user_id == order.user_id).first()
+        
+        user_address = "unknown" if not address else address.address + ',' + address.city + ',' + address.postal_code
+
+        orders_list.append({
+            "order_id": order.id,
+            "user_id": order.user_id,
+            "user_email": user_email,
+            "date": order.created_at,
+            "address": user_address,
+            "state": order.state
+        })
+        
+        print("\n\n\n\n" + str(orders_list) + "\n\n\n\n")
+
+    return {
+        "status": 200,
+        "message": "Orders list fetched successfully",
+        "orders": orders_list
     }
