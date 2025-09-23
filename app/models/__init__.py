c729d2616153615c78bbf38d4e01d9ee5d161c25
nikhilpg12 <u22504193@tuks.co.za>
2025-09-23 14:40:59 +0200

    Modifiied init.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,3 @@
 from .user import User
 from .retailer_information import RetailerInformation
 from .role import Role
-from .carbon_forecasting import (
-    CarbonForecast, CarbonForecastAccuracy, UserShoppingPattern,
-    CarbonImpactMetrics, ForecastingModelConfig
-)
