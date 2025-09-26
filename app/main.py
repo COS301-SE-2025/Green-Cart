14849610be48a40eeda98ae458efed72d29e04ad
nikhilpg12 <u22504193@tuks.co.za>
2025-09-26 18:29:11 +0200

    Hopefully done :)

--- app/main.py
+++ app/main.py
@@ -84,6 +84,8 @@ app.add_middleware(
         "https://greencart-cos301.co.za",
         "http://localhost:3000",
         "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:5174",
     ],
     allow_credentials=True,
     allow_methods=["*"],
